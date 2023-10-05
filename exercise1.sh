dfabd067b8cacdb6afb2207ead3f4487bc39c723
hochunwong <146675110+hochunwong@users.noreply.github.com>
2023-10-05 15:34:37 +0000

    am programr

--- exercise1.sh
+++ exercise1.sh
@@ -6,4 +6,17 @@ for ((i=0; i<20; i++)) do
     echo "Hello $NAME"
 done
 
-echo "Goodbye"
+for ((i=10; i>0; i--)) do
+    echo "$i"
+    sleep 0.5s
+done
+echo " > boom < "
+sleep 1s
+
+echo "did u like that?"
+read RE
+echo "why $RE"
+read RE2
+echo "why $RE2"
+read RE3
+
