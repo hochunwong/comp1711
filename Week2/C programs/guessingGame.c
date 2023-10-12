f63af3a9b7b0776aa54e45fe7eda1d512396c669
hochunwong <146675110+hochunwong@users.noreply.github.com>
2023-10-12 15:32:17 +0000

    updates to guessing game

--- Week2/C programs/guessingGame.c
+++ Week2/C programs/guessingGame.c
@@ -6,6 +6,7 @@ int main() {
     int randNum, guess, count;
     srand(time(NULL));
     randNum = rand() % 100;
+    count = 0;
 
     printf("Random number generated between 0-100\n");
     while (guess != randNum) {
@@ -16,6 +17,8 @@ int main() {
         } else if (guess<randNum) {
             printf("Higher\n");
         }
+        count = count + 1;
     }
     printf("Congrats! You got the number! It was %d!\n", randNum);
+    printf("You got it in %d guesses.\n", count);
 }
