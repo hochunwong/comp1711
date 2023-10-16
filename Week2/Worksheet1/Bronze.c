3e790f956ee2d94146e4c78369ed3672e3d3fff2
hochunwong <146675110+hochunwong@users.noreply.github.com>
2023-10-16 11:06:26 +0000

    worksheet bronze tasks

--- Week2/Worksheet1/Bronze.c
+++ Week2/Worksheet1/Bronze.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
 int main() {
+    int num;
 
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    if (num==0) {
+        printf("Number is Zero\n");
+    } else if (num>0) {
+        printf("Number is Positive\n");
+    } else {
+        printf("Number is Negative\n");
+    }
+
+    if ((num%4) == 0) {
+        printf("Number is Divisible by 4\n");
+    }
+
+    if ((num%5) == 0) {
+        printf("Number is Divisible by 5\n");
+    } 
 }
