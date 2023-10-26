2c16ed05380f1291dc7b90c8df49e2cdeff3bc0e
hochunwong <146675110+hochunwong@users.noreply.github.com>
2023-10-26 15:13:19 +0000

    struct stuff

--- Week4/Structs/struct_assign.c
+++ Week4/Structs/struct_assign.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 struct student {
-    char name [20];
+    char name [200];
     char student_id [11]; // Assuming 10 digits.
     unsigned mark;
 };
@@ -14,5 +14,18 @@ int main () {
     printf("Student name: %s\n", new_student.name);
     printf("Student ID:   %s\n", new_student.student_id);
     printf("Final mark:   %u\n", new_student.mark);
+
+    struct student second_new_student;
+    printf("\nEnter second new student's name      : ");
+    scanf("%s", second_new_student.name);
+    printf("Enter second new student's Student ID: ");
+    scanf("%s", second_new_student.student_id);
+    printf("Enter second new student's final mark: ");
+    scanf("%u", &second_new_student.mark);
+
+    printf("Student name: %s\n", second_new_student.name);
+    printf("Student ID:   %s\n", second_new_student.student_id);
+    printf("Final mark:   %u\n", second_new_student.mark);
+
     return 0;
 }
