529034068f280e05b0cbc83473eb192d09a37946
hochunwong <146675110+hochunwong@users.noreply.github.com>
2023-12-13 03:01:48 +0000

    fixing

--- Coursework2/FitnessDataSorter.c
+++ Coursework2/FitnessDataSorter.c
@@ -10,21 +10,34 @@ typedef struct {
     int steps;
 } FitnessData;
 
-// Function to tokenize a record
-void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
-    char *ptr = strtok(record, &delimiter);
-    if (ptr != NULL) {
-        strcpy(date, ptr);
-        ptr = strtok(NULL, &delimiter);
-        if (ptr != NULL) {
-            strcpy(time, ptr);
-            ptr = strtok(NULL, &delimiter);
-            if (ptr != NULL) {
-                *steps = atoi(ptr);
-            }
-        }
+
+// This is your helper function. Do not change it in any way.
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
     }
-}
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+                    }
 
 
 //Globals
@@ -37,6 +50,7 @@ int readFile() {
     int i = 0;
     char temp[100];
 
+
     printf("Enter Filename: ");
     scanf("%99s", fileName);
 
@@ -47,10 +61,10 @@ int readFile() {
         exit(1);
     }
 
-    // read file & save
+    // read file & save to array
+    i = 0;
     while (fgets(line, 100, file) != NULL) {
         tokeniseRecord(line, ",", recordArray[i].date, recordArray[i].time, temp);
-        
         recordArray[i].steps = atoi(temp);
         i = i + 1;
     }
