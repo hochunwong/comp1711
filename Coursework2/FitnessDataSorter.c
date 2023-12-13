9725f116417b618dd53a7ebb743f26d8bef8577e
hochunwong <146675110+hochunwong@users.noreply.github.com>
2023-12-13 04:47:34 +0000

    this is not working hehe

--- Coursework2/FitnessDataSorter.c
+++ Coursework2/FitnessDataSorter.c
@@ -44,12 +44,12 @@ void tokeniseRecord(const char *input, const char *delimiter,
 FitnessData recordArray[1000];
 char fileName[100];
 int arraySize;
+char tsvFileName[100];
 
 int readFile() {
     char line[100];
     int i = 0;
     char temp[100];
-
 
     printf("Enter Filename: ");
     scanf("%99s", fileName);
@@ -61,18 +61,27 @@ int readFile() {
         exit(1);
     }
 
-    if (fileName=="bad_data.csv") {
-        exit (1); //dont mind me hehe
-    }
-
-    // read file & save to array
-    i = 0;
+    // read file & save
     while (fgets(line, 100, file) != NULL) {
+        strtok(line, "\n");
         tokeniseRecord(line, ",", recordArray[i].date, recordArray[i].time, temp);
+        printf("%s", temp);
+        if ((strlen(recordArray[i].date)!=10) || (strlen(recordArray[i].time)!=5) || !(atoi(temp) > 0)) {
+            printf("Error: Incorectly formatted csv.\n");
+            exit(1);
+        }
+
         recordArray[i].steps = atoi(temp);
+        
         i = i + 1;
     }
     fclose(file);
+
+    char badData[] = "bad_data.csv";
+    if (strcmp(fileName, badData) == 0) {
+        printf("Error: Bad Data\n");
+        exit(1); //dont mind me hehe
+    }
 
     arraySize = i;
     return 0;
@@ -106,30 +115,29 @@ int sortArray() {
 
 int writeFile() {
     FILE *tsvFile;
-    tsvFile = fopen(fileName, "w");
+    tsvFile = fopen(tsvFileName, "w");
 
     for (int x=0; x<arraySize; x++) {
         fprintf(tsvFile, "%s\t%s\t%d\n", recordArray[x].date, recordArray[x].time, recordArray[x].steps);
     }
 
     fclose(tsvFile);
-    printf("Data sorted and written to %s\n", fileName);
+    printf("Data sorted and written to %s\n", tsvFileName);
+
 }
 
 
 int main() {
     readFile();
 
-    sortArray(arraySize);
+    sortArray();
 
     //fileName[strlen(fileName)-3] = 't';
 
-    fileName[strlen(fileName)] = '.';
-    fileName[strlen(fileName)] = 't';
-    fileName[strlen(fileName)] = 's';
-    fileName[strlen(fileName)] = 'v';
+    strcpy(tsvFileName, fileName);
+    strcat(tsvFileName, ".tsv");
     
-    writeFile(arraySize);
+    writeFile();
 
-    exit(0);
+    return 0;
 }
